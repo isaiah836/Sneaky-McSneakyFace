0e1ea135bad1c32aecd84b7df0b6115e0945fd88
isaiah sanchez <golfwang8363@gmail.com>
2018-06-24 23:50:49 -0700

    Start up of Sneaky McSneakFace

---
+++ Sneaky McSneakFace/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	//Singleton
+	public static GameManager instance;
+
+	public GameObject player;
+	public float moveSpeed;
+	public float turnSpeed;
+	public int lives;
+	// Use this for initialization
+	void Awake () {
+		//Checks to see if there is another GameManger
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Sneaky McSneakFace/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+	//creates a variable for the transfrom component
+	private Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check every frame if the left or right or a and d keys are pressed
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			tf.Rotate(Vector3.forward * GameManager.instance.turnSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			tf.Rotate(Vector3.back * GameManager.instance.turnSpeed * Time.deltaTime);
+		}
+
+		//CChecks every frame if the up or down or w or s keys are pressed
+		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			tf.position += tf.rotation * (Vector3.up * GameManager.instance.moveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+		{
+			tf.position += tf.rotation * (Vector2.down * GameManager.instance.moveSpeed * Time.deltaTime);
+		}
+	}
+}
