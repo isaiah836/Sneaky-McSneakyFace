33f654a6a8df70b80cf228dcde9bc0b7d42a424a
ISAIAH SANCHEZ <ISASANCH@uat.edu>
2018-06-27 13:31:21 -0700

    added stuff

---
+++ Sneaky McSneakFace/Assets/Scripts/controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class controller : MonoBehaviour {
+    public Vector3 homeposition;
+    public Pawn pawn;
+	// Use this for initialization
+	public virtual void Start () {
+        pawn = GetComponent<Pawn>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
